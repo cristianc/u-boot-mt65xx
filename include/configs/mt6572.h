897e4397af1e0c02a17e0027774768d24deca0b5
catfish03 <202436300+catfish03@users.noreply.github.com>
2025-04-20 00:49:57 +0300

    configs: mt6572: serial -> serial,button-kbd

--- include/configs/mt6572.h
+++ include/configs/mt6572.h
@@ -6,7 +6,7 @@
 #define CFG_SYS_SDRAM_BASE		0x80000000
 #define CFG_SYS_SDRAM_SIZE    0x20000000
 
-#define CFG_EXTRA_ENV_SETTINGS "stdin=serial\0" \
+#define CFG_EXTRA_ENV_SETTINGS "stdin=serial,button-kbd\0" \
 	"stdout=vidconsole,serial\0" \
 	"stderr=vidconsole,serial\0"	
 
