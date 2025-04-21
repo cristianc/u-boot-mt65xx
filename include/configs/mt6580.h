e00201acaf36aa4b709dbbd09bd286e1cc59d5cf
predefine <predefine@yandex.ru>
2025-04-21 22:09:00 +0700

    configs: add mt6580 support
    Signed-off-by: predefine <predefine@yandex.ru>

---
+++ include/configs/mt6580.h
@@ -0,0 +1,9 @@
+#ifndef __CONFIG_MT6580_H
+#define __CONFIG_MT6580_H
+
+#include <linux/sizes.h>
+
+#define CFG_SYS_SDRAM_BASE		0x80000000
+#define CFG_SYS_SDRAM_SIZE    0x40000000
+
+#endif /* ifdef __CONFIG_MT6580_H */
