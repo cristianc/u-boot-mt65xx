14c95f52929f6522c70898272d42e6279d8f4fc6
catfish03 <202436300+catfish03@users.noreply.github.com>
2025-04-23 17:05:48 +0300

    configs: mt6572: drop CFG_EXTRA_ENV_SETTINGS as we will define it in a env file

--- include/configs/mt6572.h
+++ include/configs/mt6572.h
@@ -6,8 +6,4 @@
 #define CFG_SYS_SDRAM_BASE		0x80000000
 #define CFG_SYS_SDRAM_SIZE    0x20000000
 
-#define CFG_EXTRA_ENV_SETTINGS "stdin=serial,button-kbd\0" \
-	"stdout=vidconsole,serial\0" \
-	"stderr=vidconsole,serial\0" \
-
 #endif /* ifdef __CONFIG_MT6572_H */
