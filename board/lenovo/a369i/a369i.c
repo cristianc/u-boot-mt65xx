a973a1a2d00fbb1c039861f114761faaec9cfb7c
catfish03 <catfish03@tutanota.com>
2025-05-11 05:40:31 +0300

    Add A369i board support

---
+++ board/lenovo/a369i/a369i.c
@@ -0,0 +1,18 @@
+#include <config.h>
+#include <init.h>
+#include <asm/global_data.h>
+#include <linux/sizes.h>
+#include <asm/arch/misc.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_early_init_f(void)
+{
+	gd->ram_size = get_ram_size((long *)CFG_SYS_SDRAM_BASE, SZ_512M);
+	return 0;
+}
+
+int board_init(void)
+{
+	return 0;
+}
---
+++ include/configs/a369i.h
@@ -0,0 +1,10 @@
+#ifndef __A369I_H
+#define __A369I_H
+
+#include <linux/sizes.h>
+
+/* 512 MB DRAM */
+#define CFG_SYS_SDRAM_BASE		0x80000000
+#define CFG_SYS_SDRAM_SIZE    0x20000000
+
+#endif /* ifdef __A369I_H */
