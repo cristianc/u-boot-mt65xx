6a9eca4c8233f761c16defe6679a1577d2e18940
catfish03 <catfish03@tutanota.com>
2025-05-11 05:37:40 +0300

    arm: mach-mediatek: mt6572: remove gd DRAM size init

--- arch/arm/mach-mediatek/mt6572/init.c
+++ arch/arm/mach-mediatek/mt6572/init.c
@@ -1,14 +1,13 @@
 #include <config.h>
 #include <init.h>
 #include <asm/global_data.h>
-#include <linux/sizes.h>
 #include <asm/arch/misc.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
 int dram_init(void)
 {
-	gd->ram_size = get_ram_size((long *)CFG_SYS_SDRAM_BASE, SZ_512M);
+	/* DRAM size is initialized in board files, not here */
 	return 0;
 }
 
