d3ec83ec227f4f2ba9c7babc9f99f7e7da911908
catfish03 <catfish03@tutanota.com>
2025-05-11 05:34:00 +0300

    Nuke mt6572 board and defconfig

--- include/configs/mt6572.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CONFIG_MT6572_H
-#define __CONFIG_MT6572_H
-
-#include <linux/sizes.h>
-
-#define CFG_SYS_SDRAM_BASE		0x80000000
-#define CFG_SYS_SDRAM_SIZE    0x20000000
-
-#endif /* ifdef __CONFIG_MT6572_H */
