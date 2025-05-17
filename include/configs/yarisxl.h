2844864fb84f9a76226344a511af5c2250f4d0ea
Cristian Cozzolino <cristian_ci@protonmail.com>
2025-05-18 01:49:42 +0200

    Add yarisxl board support
    Quirky. Actually, it requires setting log level options to 7 (DEBUG) to make framebuffer working.

---
+++ include/configs/yarisxl.h
@@ -0,0 +1,10 @@
+#ifndef __YARISXL_H
+#define __YARISXL_H
+
+#include <linux/sizes.h>
+
+/* 512 MB DRAM */
+#define CFG_SYS_SDRAM_BASE	0x80000000
+#define CFG_SYS_SDRAM_SIZE	0x20000000
+
+#endif /* ifdef __YARISXL_H */
